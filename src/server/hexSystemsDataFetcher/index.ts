921f9ad002b98c33d4cbe78560c2f75c31ff826f
Joey <joey.z.rp@gmail.com>
2019-06-02 11:25:54 +1000

    Extract result ids

--- src/server/hexSystemsDataFetcher/index.ts
+++ src/server/hexSystemsDataFetcher/index.ts
@@ -1,15 +1,25 @@
 import cheerio from 'cheerio';
 import fetch from 'cross-fetch';
 
+const NICK_NAME_DESC = 'nick name';
+const CLUB_DESC = 'club';
+const DEFAULT_DISCIPLINE_DESC = 'default discipline';
+const NUMBER_OF_RESULTS_DESC = 'number of results';
+const GRADE_CLUB_T_RIFLE_DESC = 'grade (club) t-rifle';
+const GRADE_CLUB_F_STD_DESC = 'grade (club) f-std';
+const GRADE_CLUB_F_OPEN_DESC = 'grade (club) f-open';
+const GRADE_CLUB_F_TR_DESC = 'grade (club) f-tr';
+
+
 const SHOOTER_INFO_DESCRIPTION_MAP = {
-    'nick name': 'nickName',
-    club: 'club',
-    'default discipline': 'defaultDiscipline',
-    'number of results': 'numberOfResult',
-    'grade (club) t-rifle': 'gradeClubTRifle',
-    'grade (club) f-std': 'gradeClubFStd',
-    'grade (club) f-open': 'gradeClubFOpen',
-    'grade (club) f-tr': 'gradeClubFTR',
+    [NICK_NAME_DESC]: 'nickName',
+    [CLUB_DESC]: 'club',
+    [DEFAULT_DISCIPLINE_DESC]: 'defaultDiscipline',
+    [NUMBER_OF_RESULTS_DESC]: 'numberOfResult',
+    [GRADE_CLUB_T_RIFLE_DESC]: 'gradeClubTRifle',
+    [GRADE_CLUB_F_STD_DESC]: 'gradeClubFStd',
+    [GRADE_CLUB_F_OPEN_DESC]: 'gradeClubFOpen',
+    [GRADE_CLUB_F_TR_DESC]: 'gradeClubFTR',
 };
 
 const fetcher = () => {
@@ -31,9 +41,19 @@ const fetcher = () => {
                 const value = $(row).children('td').text();
                 shooterInfo[key] = value;
             });
+
+            const resultTable = $('tbody', 'div#short-shootings-list');
+            const resultRows = resultTable.find('tr');
+            const resultIds: string[] = resultRows.map((index, row) => ($(row).attr('id').match(/shooting-(.*)/) || [])[1]).get();
+            
+            const numberOfResult = parseInt(shooterInfo[SHOOTER_INFO_DESCRIPTION_MAP[NUMBER_OF_RESULTS_DESC]], 10);
+            if (numberOfResult !== resultIds.length) {
+                throw new Error('Number of result rows does not match result ids');
+            }
             
             return {
                 name,
+                resultIds,
                 ...shooterInfo,
             };
         });
